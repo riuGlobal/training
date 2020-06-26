4bfc9d951d7b0c8072074a7d9b06f0908758bb5c
ricardo <ricardo@riunturi.riunturi>
2020-06-25 22:07:31 -0500

    http exception to have consistent message of error

--- src/http-exception-filter.ts
+++ src/http-exception-filter.ts
@@ -3,17 +3,20 @@ import { Request, Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) :void {
+  catch (exception: HttpException, host: ArgumentsHost) :void {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
 
+    const responseException: Record<string, unknown> = exception.getResponse() as Record<string, unknown>;
+
     response
       .status(status)
       .json({
         statusCode: status,
-        message: exception.message
+        error: exception.message,
+        ...(!!responseException.error) && { message: responseException }
       });
   }
 }
