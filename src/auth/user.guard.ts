ca9e15448813427e21f6b1f7a05b4a8570ae9db0
riuGlobal <47646835+riuGlobal@users.noreply.github.com>
2020-06-28 18:35:28 -0500

    Feature/guard forbidden (#1)
    Created guard to be used in endpoint with uid header. If no uid header then throw Forbidden exception.
    
    Co-authored-by: ricardo <ricardo@riunturi.riunturi>

---
+++ src/auth/user.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from '@nestjs/common';
+import { Request } from 'express';
+
+@Injectable()
+export class UserGuard implements CanActivate {
+  canActivate (
+    context: ExecutionContext
+  ): boolean {
+    const request = context.switchToHttp().getRequest<Request>();
+    if (!this.validateRequest(request)) { throw new ForbiddenException(); }
+    return true;
+  }
+
+  validateRequest (
+    request: Request
+  ): boolean {
+    return !!request.get('uid');
+  }
+}
