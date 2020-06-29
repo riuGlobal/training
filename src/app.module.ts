a4c0d702597daa3fc6a415c5efc72761341a031c
ricardo <ricardo@riunturi.riunturi>
2020-06-28 23:11:50 -0500

    Module trainees

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TraineesModule } from './trainees/trainees.module';
 
 @Module({
-  imports: [],
+  imports: [TraineesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
