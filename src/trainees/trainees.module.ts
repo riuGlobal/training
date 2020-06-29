c97c41fb6fdc30173ffd4400fbc129765e636e07
ricardo <ricardo@riunturi.riunturi>
2020-06-28 23:21:19 -0500

    Added service

--- src/trainees/trainees.module.ts
+++ src/trainees/trainees.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TraineesController } from './trainees.controller';
+import { TraineesService } from './trainees.service';
 
 @Module({
-  controllers: [TraineesController]
+  controllers: [TraineesController],
+  providers: [TraineesService]
 })
 export class TraineesModule {}
