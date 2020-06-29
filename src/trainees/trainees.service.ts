dcc3abcbe879510bce46b4b82fa34416eec09536
ricardo <ricardo@riunturi.riunturi>
2020-06-29 00:58:19 -0500

    Added index method in trainee service

--- src/trainees/trainees.service.ts
+++ src/trainees/trainees.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Trainee } from './trainee.entity';
 
 @Injectable()
-export class TraineesService {}
+export class TraineesService {
+  constructor (
+      private traineeRepository: Repository<Trainee>
+  ) {}
+
+  async index (uid: string): Promise<Trainee> {
+    return await this.traineeRepository.findOne(uid);
+  }
+
+  //store
+    //Create many or just one given array
+
+  //update
+    // update just one given entire thing or partialy with patch
+  //delete
+    //delete just one
+}
