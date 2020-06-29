ea9937b1fbdfb9a3569685abe2aae13d4954dad5
ricardo <ricardo@riunturi.riunturi>
2020-06-28 23:19:48 -0500

    Added trainee controller

--- src/trainees/trainees.module.ts
+++ src/trainees/trainees.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { TraineesController } from './trainees.controller';
 
-@Module({})
+@Module({
+  controllers: [TraineesController]
+})
 export class TraineesModule {}
