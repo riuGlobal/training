c997b9ca60f63094ec9044dd0e985974c28d68d8
ricardo <ricardo@riunturi.riunturi>
2020-06-28 23:17:57 -0500

    Linted: COmmented useless declaration

--- src/http-exception-filter.ts
+++ src/http-exception-filter.ts
@@ -1,12 +1,12 @@
 import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch (exception: HttpException, host: ArgumentsHost) :void {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
+    // const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
 
     const responseException: Record<string, unknown> = exception.getResponse() as Record<string, unknown>;
