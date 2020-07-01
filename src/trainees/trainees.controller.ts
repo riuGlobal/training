fd95127d9dadb66a99aefad005e682186fef36a6
ricardo <ricardo@riunturi.riunturi>
2020-06-30 22:18:19 -0500

    Added GET /trainees endpoint

--- src/trainees/trainees.controller.ts
+++ src/trainees/trainees.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Headers } from '@nestjs/common';
+import { TraineesService } from './trainees.service';
+import { Trainee } from './trainee.entity';
 
 @Controller('trainees')
-export class TraineesController {}
+export class TraineesController {
+  constructor (
+    private readonly traineeService: TraineesService
+  ) {}
+
+  @Get()
+  async show (@Headers('uid') uid :string): Promise<Trainee> {
+    return this.traineeService.show(uid);
+  }
+}
