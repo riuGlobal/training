8e5d8a550aa40cd46a283d10ccf346e9f49a6ff3
riuGlobal <47646835+riuGlobal@users.noreply.github.com>
2020-07-08 19:42:10 -0500

    Update global.decorator.ts

--- src/decorators/global.decorator.ts
+++ src/decorators/global.decorator.ts
@@ -1,7 +1,6 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const Header = createParamDecorator(
   (data: string, ctx: ExecutionContext) => {
-
     const request = ctx.switchToHttp().getRequest();
     return request.headers[data];
   }
