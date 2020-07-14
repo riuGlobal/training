105ca7767cf64ce35ada0f3724f0b032d99668b0
ricardo <ricardo@riunturi.riunturi>
2020-07-13 21:48:03 -0500

    Added unique validation

--- src/exercises/exercise.entity.ts
+++ src/exercises/exercise.entity.ts
@@ -1,11 +1,13 @@
 import { Entity, PrimaryGeneratedColumn, Column, Index } from 'typeorm';
 import { IsNotEmpty } from 'class-validator';
+import { IsUniqueIndex } from '../validators/repository/repository.unique';
 
 @Entity('exercise')
 export class Exercise {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @IsUniqueIndex(Exercise)
   @IsNotEmpty()
   @Index({ unique: true })
   @Column()
